814fb1c0b687d0dee12f375dc306e253a86e18c9
Hanzala <hanzalaahmed6718@gmail.com>
2024-01-10 21:12:42 +0500

    Final Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,23 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:rental_app/features/terms_and_conditions/views/term_and_conditions_screen.dart';
-import 'package:rental_app/features/terms_and_conditions/views/term_and_conditions_screen.dart';
+import 'package:rental_app/features/auth/view/signin_screen.dart';
 import 'package:rental_app/features/terms_and_conditions/views/term_and_conditions_screen.dart';
 import 'package:rental_app/routes/route_manager.dart';
 import 'package:rental_app/utils/constants/app_constants.dart';
 import 'package:rental_app/utils/thems/theme.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'commons/common_imports/common_libs.dart';
+import 'core/shared_prefs_service/shared_prefs_service.dart';
 import 'features/auth/controller/auth_controller.dart';
-import 'features/auth/view/signin_screen.dart';
 import 'features/main_menu/views/main_menu_screen.dart';
 import 'firebase_options.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 
+final sharedPreferencesProvider = Provider<SharedPreferences>((ref) {
+  throw UnimplementedError();
+});
 
 void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
@@ -34,10 +37,25 @@ class MyApp extends ConsumerStatefulWidget {
 
 class _MyAppState extends ConsumerState<MyApp> {
 
+  bool isNewUser = true;
+
   @override
   void initState() {
     super.initState();
+    initiallize();
     FlutterNativeSplash.remove();
+  }
+
+  initiallize()async{
+    final sharedPreferences = await SharedPreferences.getInstance();
+    ISharefPrefsController sharedPrefController = ISharefPrefsController(
+        sharedPreferences: await SharedPreferences.getInstance());
+    String? val = sharedPrefController.getUserStatus();
+    if(val != null){
+      isNewUser = false;
+    }else{
+      isNewUser = true;
+    }
   }
 
   @override
@@ -72,13 +90,18 @@ class _MyAppState extends ConsumerState<MyApp> {
                 if (user != null) {
                   return  const MainMenuScreen();
                 } else {
-                  return const TermsAndConditionScreen();
+                  // String? val = ref.read(sharedPrefsCtr).getUserStatus();
+                  if(!isNewUser){
+                    return const SignInScreen();
+                  }else{
+                    return const TermsAndConditionScreen();
+                  }
                 }
               },
               error: (error, st) => const TermsAndConditionScreen(),
               loading: () => const TermsAndConditionScreen()),
         );
-      }, 
+      },
     );
   }
 }
